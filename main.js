ddf5db40ce51d7041c28fd74958772eed414a870
Caelus11 <wmaarouf660@gmail.com>
2023-12-15 13:04:05 +0100

    changed some things

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const title = document.querySelector('.title')
-const advice = document.querySelector('.advice')
-const dice = document.querySelector('.dice')
-const apiUrl = 'https://api.adviceslip.com/advice'
-
+const title = document.querySelector(".title");
+const advice = document.querySelector(".advice");
+const dice = document.querySelector(".dice");
+const apiUrl = "https://api.adviceslip.com/advice";
 
 function getAdvice(params) {
-    fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => {
-        title.textContent = `advice #${data.slip.id}`
-        advice.textContent = `${data.slip.advice}`
-    })
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      title.textContent = `advice #${data.slip.id}`;
+      advice.textContent = `"${data.slip.advice}"`;
+    });
 }
+getAdvice();
 
-getAdvice()
-
-dice.addEventListener('click', getAdvice)
+dice.addEventListener("click", getAdvice);
